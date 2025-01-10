d8c9cc6d66342c7d9667fa3f6caecd2c49c1187a
Evan Relf <evan@evanrelf.com>
2025-01-09 22:29:18 -0800

    Allow customizing units

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::Context;
 use clap::Parser as _;
+use std::fmt::Display;
 
 #[derive(clap::Parser, Debug)]
 struct Args {
@@ -11,7 +12,9 @@ struct Args {
     #[clap(long, env = "FORECAST_LONGITUDE", allow_hyphen_values = true)]
     lon: f32,
 
-    /// User-Agent
+    #[clap(long, env = "FORECAST_UNITS", default_value_t = Units::Us)]
+    units: Units,
+
     #[clap(long, env = "FORECAST_USER_AGENT")]
     user_agent: String,
 }
@@ -33,7 +36,10 @@ fn main() -> anyhow::Result<()> {
         .context("could not decode point")?;
 
     let forecast_res: GeoJson<GridpointForecast> = client
-        .get(point_res.properties.forecast)
+        .get(format!(
+            "{}?units={}",
+            point_res.properties.forecast, args.units
+        ))
         .header("user-agent", &args.user_agent)
         .send()
         .context("request for forecast failed")?
@@ -51,6 +57,23 @@ fn main() -> anyhow::Result<()> {
 }
 
 // https://www.weather.gov/documentation/services-web-api
+
+#[derive(clap::ValueEnum, Clone, Copy, Debug, serde::Serialize)]
+enum Units {
+    /// Imperial (ºF, mph)
+    Us,
+    /// Metric (ºC, km/h)
+    Si,
+}
+
+impl Display for Units {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Us => write!(f, "us"),
+            Self::Si => write!(f, "si"),
+        }
+    }
+}
 
 #[derive(serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
