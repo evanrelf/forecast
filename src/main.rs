b8d4a7570231c4287f58b795441169e55491bd6c
Evan Relf <evan@evanrelf.com>
2025-01-09 20:55:52 -0800

    Get weather forecast from `api.weather.gov`

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,65 @@
+use anyhow::Context;
 use clap::Parser as _;
 
 #[derive(clap::Parser, Debug)]
-struct Args {}
+struct Args {
+    /// Latitude
+    #[clap(long, allow_hyphen_values = true)]
+    lat: f32,
 
-fn main() {
+    /// Longitude
+    #[clap(long, allow_hyphen_values = true)]
+    lon: f32,
+
+    /// User-Agent
+    #[clap(long)]
+    user_agent: String,
+}
+
+fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
-    println!("{args:?}");
+    let client = reqwest::blocking::Client::new();
+
+    let point_url = format!("https://api.weather.gov/points/{},{}", args.lat, args.lon);
+
+    let point_res: serde_json::Value = client
+        .get(point_url)
+        .header("user-agent", &args.user_agent)
+        .send()
+        .context("failed to request point")?
+        .json()
+        .context("failed to decode point")?;
+
+    let forecast_url = point_res
+        .get("properties")
+        .context("point missing 'properties' key")?
+        .get("forecast")
+        .context("point missing 'properties.forecast' key")?
+        .as_str()
+        .context("point 'properties.forecast' is not string")?;
+
+    let forecast_res: serde_json::Value = client
+        .get(forecast_url)
+        .header("user-agent", &args.user_agent)
+        .send()
+        .context("failed to request forecast")?
+        .json()
+        .context("failed to decode forecast")?;
+
+    let forecast_text: &str = forecast_res
+        .get("properties")
+        .context("forecast missing 'properties' key")?
+        .get("periods")
+        .context("forecast missing 'properties.periods' key")?
+        .get(0)
+        .context("forecast missing 'properties.periods.[0]' element")?
+        .get("detailedForecast")
+        .context("forecast missing 'properties.periods.[0].detailedForecast' key")?
+        .as_str()
+        .context("forecast 'properties.periods.[0].detailedForecast' is not string")?;
+
+    println!("{forecast_text}");
+
+    Ok(())
 }
