2427d1340075f894da25be2312a7b9b80cf04c36
Evan Relf <evan@evanrelf.com>
2025-01-09 21:12:10 -0800

    Lean on `serde` for parsing JSON

--- src/main.rs
+++ src/main.rs
@@ -21,45 +21,57 @@ fn main() -> anyhow::Result<()> {
 
     let client = reqwest::blocking::Client::new();
 
-    let point_url = format!("https://api.weather.gov/points/{},{}", args.lat, args.lon);
-
-    let point_res: serde_json::Value = client
-        .get(point_url)
+    let point_res: GeoJson<Point> = client
+        .get(format!(
+            "https://api.weather.gov/points/{},{}",
+            args.lat, args.lon
+        ))
         .header("user-agent", &args.user_agent)
         .send()
         .context("failed to request point")?
         .json()
         .context("failed to decode point")?;
 
-    let forecast_url = point_res
-        .get("properties")
-        .context("point missing 'properties' key")?
-        .get("forecast")
-        .context("point missing 'properties.forecast' key")?
-        .as_str()
-        .context("point 'properties.forecast' is not string")?;
-
-    let forecast_res: serde_json::Value = client
-        .get(forecast_url)
+    let forecast_res: GeoJson<GridpointForecast> = client
+        .get(point_res.properties.forecast)
         .header("user-agent", &args.user_agent)
         .send()
         .context("failed to request forecast")?
         .json()
         .context("failed to decode forecast")?;
 
-    let forecast_text: &str = forecast_res
-        .get("properties")
-        .context("forecast missing 'properties' key")?
-        .get("periods")
-        .context("forecast missing 'properties.periods' key")?
-        .get(0)
-        .context("forecast missing 'properties.periods.[0]' element")?
-        .get("detailedForecast")
-        .context("forecast missing 'properties.periods.[0].detailedForecast' key")?
-        .as_str()
-        .context("forecast 'properties.periods.[0].detailedForecast' is not string")?;
+    let forecast_text = &forecast_res
+        .properties
+        .periods
+        .first()
+        .context("forecast has no periods")?
+        .detailed_forecast;
 
     println!("{forecast_text}");
 
     Ok(())
+}
+
+#[derive(serde::Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct GeoJson<T> {
+    properties: T,
+}
+
+#[derive(serde::Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Point {
+    forecast: String,
+}
+
+#[derive(serde::Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct GridpointForecast {
+    periods: Vec<GridpointForecastPeriod>,
+}
+
+#[derive(serde::Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct GridpointForecastPeriod {
+    detailed_forecast: String,
 }
